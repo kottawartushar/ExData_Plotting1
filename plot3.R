8c1fc980dc0b8664a2df8cfd14d397eece0db711
Kottawar <Tushar.Kottawar@Honeywell.com>
2020-06-27 22:01:17 +0530

    plot3.R and plot3.R uploaded

---
+++ plot3.R
@@ -0,0 +1,14 @@
+read_file <- read.table("household_power_consumption.txt", header = TRUE, 
+                        sep = ";", stringsAsFactors = FALSE, na.strings = "?")
+##head(read_file)
+subsetDate_data <- subset(read_file, Date %in% c("1/2/2007", "2/2/2007"))
+subsetDate_data$Date <- as.Date(subsetDate_data$Date, format = "%d/%m/%Y")
+subsetDate_data$date_time <- as.POSIXct(paste(subsetDate_data$Date, subsetDate_data$Time))
+plot(subsetDate_data$date_time, subsetDate_data$Sub_metering_1, type = "l",
+     ylab = "Energy sub metering")
+lines(subsetDate_data$date_time, subsetDate_data$Sub_metering_2, type = "l",
+      ylab = "Energy sub metering", col = "red")
+lines(subsetDate_data$date_time, subsetDate_data$Sub_metering_3, type = "l",
+      ylab = "Energy sub metering", col = "blue")
+png("plot3.png", width = 480, height = 480)
+dev.off()
