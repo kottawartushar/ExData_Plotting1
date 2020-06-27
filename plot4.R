0b1cee823db11aa63e5815bd606b0dc05850269e
Kottawar <Tushar.Kottawar@Honeywell.com>
2020-06-27 23:24:53 +0530

    plot4.R with changes

--- plot4.R
+++ plot4.R
@@ -1,6 +1,6 @@
 read_file <- read.table("household_power_consumption.txt", header = TRUE, 
                         sep = ";", stringsAsFactors = FALSE, na.strings = "?")
-##head(read_file)
+
 subsetDate_data <- subset(read_file, Date %in% c("1/2/2007", "2/2/2007"))
 subsetDate_data$Date <- as.Date(subsetDate_data$Date, format = "%d/%m/%Y")
 subsetDate_data$date_time <- as.POSIXct(paste(subsetDate_data$Date, subsetDate_data$Time))
