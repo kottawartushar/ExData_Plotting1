57e6f1a4be79e8c5733894ac5470a244ccfefea9
Kottawar <Tushar.Kottawar@Honeywell.com>
2020-06-27 21:04:31 +0530

    removed an extra line from the code

--- plot2.R
+++ plot2.R
@@ -3,7 +3,6 @@ read_file <- read.table("household_power_consumption.txt", header = TRUE,
 ##head(read_file)
 subsetDate_data <- subset(read_file, Date %in% c("1/2/2007", "2/2/2007"))
 subsetDate_data$Date <- as.Date(subsetDate_data$Date, format = "%d/%m/%Y")
-##subsetDate_data$Time <- strptime(subsetDate_data$Time, format = "%H:%M:%S")
 subsetDate_data$date_time <- as.POSIXct(paste(subsetDate_data$Date, subsetDate_data$Time))
 plot(subsetDate_data$date_time, subsetDate_data$Global_active_power, type = "l", xlab = " ",
      ylab = "Global Active Power (kilowatts)")
