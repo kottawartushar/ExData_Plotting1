5b4d6cbc11e3269bb33d75b05678f30388ad5c51
Kottawar <Tushar.Kottawar@Honeywell.com>
2020-06-27 22:07:35 +0530

    plot3.R uploaded with changes

--- plot3.R
+++ plot3.R
@@ -10,5 +10,7 @@ lines(subsetDate_data$date_time, subsetDate_data$Sub_metering_2, type = "l",
       ylab = "Energy sub metering", col = "red")
 lines(subsetDate_data$date_time, subsetDate_data$Sub_metering_3, type = "l",
       ylab = "Energy sub metering", col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lty = 1, lwd = 1.5, col = c("black", "red", "blue"))
 png("plot3.png", width = 480, height = 480)
 dev.off()
