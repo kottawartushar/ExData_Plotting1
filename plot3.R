d4f62cffd1a2a019dc2210b0cc3c49aa2579f0db
Kottawar <Tushar.Kottawar@Honeywell.com>
2020-06-27 22:56:39 +0530

    plot3.R updated

--- plot3.R
+++ plot3.R
@@ -4,13 +4,14 @@ read_file <- read.table("household_power_consumption.txt", header = TRUE,
 subsetDate_data <- subset(read_file, Date %in% c("1/2/2007", "2/2/2007"))
 subsetDate_data$Date <- as.Date(subsetDate_data$Date, format = "%d/%m/%Y")
 subsetDate_data$date_time <- as.POSIXct(paste(subsetDate_data$Date, subsetDate_data$Time))
-plot(subsetDate_data$date_time, subsetDate_data$Sub_metering_1, type = "l",
+plot(subsetDate_data$date_time, subsetDate_data$Sub_metering_1, type = "l", xlab = "",
      ylab = "Energy sub metering")
 lines(subsetDate_data$date_time, subsetDate_data$Sub_metering_2, type = "l",
       ylab = "Energy sub metering", col = "red")
 lines(subsetDate_data$date_time, subsetDate_data$Sub_metering_3, type = "l",
       ylab = "Energy sub metering", col = "blue")
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       lty = 1, lwd = 1.5, col = c("black", "red", "blue"))
+legend("topright", lty = 1,
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col = c("black", "red", "blue"))
 png("plot3.png", width = 480, height = 480)
 dev.off()
